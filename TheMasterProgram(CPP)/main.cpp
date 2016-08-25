a5be33ad452eb2e405cad43f12597f05bb31b437
Jarrod Raine <jarrodfraine@me.com>
2016-08-25 12:13:29 -0600

    Initial Commit

---
+++ TheMasterProgram(CPP)/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  TheMasterProgram(CPP)
+//
+//  Created by Jarrod Raine on 8/25/16.
+//  Copyright © 2016 Jarrod Raine. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
